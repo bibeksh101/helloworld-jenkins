de029becfa2ec4705ac476b81a42816df65771d5
bibeksh101 <48139921+bibeksh101@users.noreply.github.com>
2021-09-07 13:47:49 -0700

    Harry Potter Added

--- helloworld.java
+++ helloworld.java
@@ -2,5 +2,6 @@ class helloworld {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
         System.out.println("Lorem Ipsum!"); 
+        System.out.println("You are a Wizard Harry!!"); 
     }
 }
