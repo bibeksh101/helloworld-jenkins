b67378a84ed1e902a3e83999d226639cc245aec1
bibeksh101 <bibek.sh101@gmail.com>
2022-03-31 16:29:38 -0700

    new file added

--- helloworld.java
+++ helloworld.java
@@ -1,8 +1,8 @@
 class helloworld {
     public static void main(String[] args) {
         System.out.println("Hello, World 2 this!"); 
-        System.out.println("Lorem Ipsum!"); 
-        System.out.println("You are a Wizard Harry!!");
+        System.out.println("Lorem Ipsum 3 this!"); 
+        System.out.println("You are a Wizard Harry 4 this!!");
         System.out.println("Lorem Ipsum!"); 
     }
 }
